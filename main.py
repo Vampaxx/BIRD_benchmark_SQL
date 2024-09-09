2e73fcb8bfd0c14fb29685af170f6e765a11d6c6
Vampaxx <arjunappu1001@gmail.com>
2024-09-09 19:05:57 +0530

    Data splitting stage [main.py]

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Bird_bench_SQL import logger
 from Bird_bench_SQL.pipeline.stage_1_data_ingestion import DataIngestionPipeline
+from Bird_bench_SQL.pipeline.stage_02_data_splitting import DataSplittingPipeline
+
 
 
 STAGE_NAME = "Data ingestion stage"
@@ -12,3 +14,13 @@ try:
 except Exception as e:
     raise e
 
+STAGE_NAME = "Data splitting stage"
+try:
+    logger.info(f"<<<    stage   {STAGE_NAME}    started >>>")
+    obj = DataSplittingPipeline()
+    obj.main()
+    logger.info(f"<<<    stage   {STAGE_NAME}    starcompleted \n\n===========================================>>>")
+
+except Exception as e:
+    raise e
+        
